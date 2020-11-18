b2013110ec444a9f66da85e6ee0ccb84320e710d
antelk <alojic00@fesb.hr>
2020-11-18 22:33:22 +0100

    new simulation data

--- src/generate_dataset.m
+++ src/generate_dataset.m
@@ -1,8 +1,8 @@
 set_input;
 
+%% multiple temperatures
 ks = [0.001, 0.1, 0.3, 0.9];
 Ts = linspace(1, 30, 30);
-
 tic;
 for i = 1:length(Ts)
     T = Ts(i);
@@ -17,6 +17,25 @@ for i = 1:length(Ts)
                      '_Itstop-', num2str(t_stop), '_T-', num2str(T), ...
                      '_k-', num2str(k), '.csv']));
         end
+    end
+end
+toc;
+
+
+%% multiple magnetic flux coeffs for fixed T
+ks = linspace(0.0016, 3.6, 100);
+T = 16.3;
+tic;
+for j = 1:length(ks)
+    k = ks(j);
+    [t, y] = ode45(@HodgkinHuxley, t_span, y0);
+
+    if save_data
+        writematrix(["t", "V", "m", "h", "n", "phi"; t, y], ...
+            fullfile(datadir, ...
+                ['tVmhnPhi', '_t-', num2str(t_span(2)), ...
+                 '_Itstop-', num2str(t_stop), '_T-', num2str(T), ...
+                 '_k-', num2str(k), '.csv']));
     end
 end
 toc;
