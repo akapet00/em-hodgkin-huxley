91bb8b1ad4fa0dc1197aafed1136ea31a22499c9
antelk <alojic00@fesb.hr>
2020-12-01 13:12:58 +0100

    fix CI plotting

--- src/output/sensitivity_analysis/sc_output_postprocessing/gen_figs.py
+++ src/output/sensitivity_analysis/sc_output_postprocessing/gen_figs.py
@@ -9,7 +9,7 @@ from scipy.io import loadmat
 DATA_PATH = 'tsim-300_tIinjstop-300_T-15_k-0-2'
 K_LIST = np.linspace(0, 2, 50)
 SCP_LIST = [3, 5, 7, 9]
-VAR_LIST = [5, 10, 20, 50]
+CV_LIST = [5, 10, 20, 50]
 
 
 def plotting_config(nrows=1, ncols=1):
@@ -35,27 +35,27 @@ def load_data(ci, sc):
 
 
 def multiview(savefig=False):
-    nrows = len(VAR_LIST)
+    nrows = len(CV_LIST)
     ncols = len(SCP_LIST)
     plotting_config(nrows, ncols)
     fig, ax = plt.subplots(nrows, ncols, sharex=True, sharey=True,
         squeeze=True)
-    for ci_idx, ci in enumerate(VAR_LIST):
+    for cv_idx, cv in enumerate(CV_LIST):
         for sc_idx, sc in enumerate(SCP_LIST):
-            data = load_data(ci, sc) 
+            data = load_data(cv, sc) 
             mean_mISI = data[0]['M'].ravel()
             var_mISI = data[1]['V'].ravel()
             lb = mean_mISI - np.sqrt(var_mISI)
             ub = mean_mISI + np.sqrt(var_mISI)
-            ax[ci_idx, sc_idx].plot(K_LIST, mean_mISI, color='black',
-                label=f'$\\langle mISI \\rangle$, {sc} SC points')
-            ax[ci_idx, sc_idx].fill_between(K_LIST, lb, ub, color='lightgray',
-                label=f'{ci}% CI')
-            if ci_idx == nrows-1:
-                ax[ci_idx, sc_idx].set_xlabel('$k$')
+            ax[cv_idx, sc_idx].plot(K_LIST, mean_mISI, color='black',
+                label=f'$\\langle mISI \\rangle$, ${sc}$ SC points, $\\alpha = {cv}$%')
+            ax[cv_idx, sc_idx].fill_between(K_LIST, lb, ub, color='lightgray',
+                label='95% CI')
+            if cv_idx == nrows-1:
+                ax[cv_idx, sc_idx].set_xlabel('$k$')
             if sc_idx == 0:
-                ax[ci_idx, sc_idx].set_ylabel('$mISI (k)$ [ms]')
-            ax[ci_idx, sc_idx].legend(loc='best')
+                ax[cv_idx, sc_idx].set_ylabel('$mISI (k)$ [ms]')
+            ax[cv_idx, sc_idx].legend(loc='best')
     plt.tight_layout()
     plt.show()
     if savefig:
@@ -65,24 +65,25 @@ def multiview(savefig=False):
 
 def sc_convergence(savefig=False):
     figmarks = ['ko-', 'ks-', 'k^:', 'kv:']
-    nfigs = len(VAR_LIST)
+    nfigs = len(CV_LIST)
     plotting_config(nfigs, 2)
     fig, ax = plt.subplots(2, nfigs, sharex=True, squeeze=True)
-    for ci_idx, ci in enumerate(VAR_LIST):
+    for cv_idx, cv in enumerate(CV_LIST):
         for sc_idx, sc in enumerate(SCP_LIST):
-            data = load_data(ci, sc) 
+            data = load_data(cv, sc) 
             mean_mISI = data[0]['M'].ravel()
             var_mISI = data[1]['V'].ravel()
-            ax[0, ci_idx].plot(K_LIST, mean_mISI, figmarks[sc_idx], markevery=5,
-                label=f'$\\langle mISI \\rangle$, {sc} SC points')
-            ax[1, ci_idx].plot(K_LIST, var_mISI, figmarks[sc_idx], markevery=5,
-                label=f'$Var(mISI)$, {sc} SC pts')
-            ax[1, ci_idx].set_xlabel('$k$')
-            if ci_idx == 0:
-                ax[0, ci_idx].set_ylabel('$mISI (k)$ [ms]')
-                ax[1, ci_idx].set_ylabel('$Var(mISI)(k)$ [ms]')
-            ax[0, ci_idx].legend(loc='best')
-            ax[1, ci_idx].legend(loc='best')           
+            ax[0, cv_idx].plot(K_LIST, mean_mISI, figmarks[sc_idx], markevery=5,
+                label=f'$\\langle mISI \\rangle$, ${sc}$ SC points')
+            ax[0, cv_idx].set_title(f'$\\alpha = {cv}$%')
+            ax[1, cv_idx].plot(K_LIST, var_mISI, figmarks[sc_idx], markevery=5,
+                label=f'$Var(mISI)$, ${sc}$ SC pts')
+            ax[1, cv_idx].set_xlabel('$k$')
+            if cv_idx == 0:
+                ax[0, cv_idx].set_ylabel('$mISI (k)$ [ms]')
+                ax[1, cv_idx].set_ylabel('$Var(mISI)(k)$ [ms]')
+            ax[0, cv_idx].legend(loc='best')
+            ax[1, cv_idx].legend(loc='best')           
     plt.tight_layout()
     plt.show()
     if savefig:
@@ -94,26 +95,26 @@ def anova(savefig=False):
     from cycler import cycler
     plt.rcParams.update({
         'axes.prop_cycle': cycler('linestyle', ['-', '--', ':'])})
-    nfigs = len(VAR_LIST)
+    nfigs = len(CV_LIST)
     plotting_config(nfigs, 3)
     fig, ax = plt.subplots(3, nfigs, sharex=True, sharey=True, squeeze=True)
     sc = 5
-    for ci_idx, ci in enumerate(VAR_LIST):
+    for cv_idx, cv in enumerate(CV_LIST):
         data = loadmat(os.path.join(DATA_PATH, 
-            f'SA_ANOVA_indices_CI{ci}_SC_{sc}.mat'))['SS']
-        ax[0, ci_idx].plot(K_LIST, data[:, :3])
-        ax[0, ci_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'])
-        ax[0, ci_idx].set_title(f'{ci}% CI, {sc} SC points')
-        ax[1, ci_idx].plot(K_LIST, data[:, 3:6])
-        ax[1, ci_idx].legend(['$g_{Na}$ $g_{K}$', '$g_{Na}$ $g_{L}$',
+            f'SA_ANOVA_indices_CI{cv}_SC_{sc}.mat'))['SS']
+        ax[0, cv_idx].plot(K_LIST, data[:, :3])
+        ax[0, cv_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'])
+        ax[0, cv_idx].set_title(f'${sc}$ SC points, $\\alpha = {cv}$%')
+        ax[1, cv_idx].plot(K_LIST, data[:, 3:6])
+        ax[1, cv_idx].legend(['$g_{Na}$ $g_{K}$', '$g_{Na}$ $g_{L}$',
             '$g_{K}$ $g_{L}$'])
-        ax[2, ci_idx].plot(K_LIST, data[:, 6:])
-        ax[2, ci_idx].set_xlabel('$k$')
-        ax[2, ci_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'], loc='lower right')
-        if ci_idx == 0:
-            ax[0, ci_idx].set_ylabel('$1^{st}$ order sensitivity')
-            ax[1, ci_idx].set_ylabel('$2^{nd}$ order sensitivity')
-            ax[2, ci_idx].set_ylabel('total effect sensitivity')
+        ax[2, cv_idx].plot(K_LIST, data[:, 6:])
+        ax[2, cv_idx].set_xlabel('$k$')
+        ax[2, cv_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'], loc='lower right')
+        if cv_idx == 0:
+            ax[0, cv_idx].set_ylabel('$1^{st}$ order sensitivity')
+            ax[1, cv_idx].set_ylabel('$2^{nd}$ order sensitivity')
+            ax[2, cv_idx].set_ylabel('total effect sensitivity')
     plt.tight_layout()
     plt.show()
     if savefig:
