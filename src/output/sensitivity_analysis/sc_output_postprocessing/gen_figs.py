54c0d72bec5717e3ad658721ba93628aca7cbdb8
antelk <alojic00@fesb.hr>
2020-12-16 10:07:31 +0100

    subtile improvements

--- src/output/sensitivity_analysis/sc_output_postprocessing/gen_figs.py
+++ src/output/sensitivity_analysis/sc_output_postprocessing/gen_figs.py
@@ -23,7 +23,6 @@ def plotting_config(nrows=1, ncols=1):
         'xtick.labelsize': 8,
         'ytick.labelsize': 8,
         'figure.figsize': [4.774*nrows, 2.950*ncols],
-        'figure.max_open_warning': 120,
     })
 
 
@@ -32,7 +31,7 @@ def load_data(ci, sc):
     var_mat = loadmat(os.path.join(DATA_PATH, f'VAR_CI{ci}_SC_{sc}.mat'))
     skew_mat = loadmat(os.path.join(DATA_PATH, f'SKEW_CI{ci}_SC_{sc}.mat'))
     kurt_mat = loadmat(os.path.join(DATA_PATH, f'KURT_CI{ci}_SC_{sc}.mat'))
-    return mean_mat, var_mat, skew_mat, kurt_mat
+    return (mean_mat, var_mat, skew_mat, kurt_mat)
 
 
 def multiview(showfig=True, savefig=False):
@@ -48,10 +47,8 @@ def multiview(showfig=True, savefig=False):
             var_mISI = data[1]['V'].ravel()
             lb = mean_mISI - np.sqrt(var_mISI)
             ub = mean_mISI + np.sqrt(var_mISI)
-            ax[cv_idx, sc_idx].plot(K_LIST, mean_mISI, color='black',
-                label=f'$\\langle mISI \\rangle$, ${sc}$ SC points, $\\alpha = {cv}$%')
-            ax[cv_idx, sc_idx].fill_between(K_LIST, lb, ub, color='lightgray',
-                label='95% CI')
+            ax[cv_idx, sc_idx].plot(K_LIST, mean_mISI, color='black', label=f'$\\langle mISI \\rangle$, ${sc}$ SC points, $\\alpha = {cv}$%')
+            ax[cv_idx, sc_idx].fill_between(K_LIST, lb, ub, color='lightgray', label='95% CI')
             if cv_idx == nrows-1:
                 ax[cv_idx, sc_idx].set_xlabel('$k$')
             if sc_idx == 0:
@@ -61,8 +58,7 @@ def multiview(showfig=True, savefig=False):
     if showfig:
         plt.show()
     if savefig:
-        fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_multiview.eps'),
-            format='eps', bbox_inches='tight')
+        fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_multiview.eps'), format='eps', bbox_inches='tight')
 
 
 def sc_convergence(showfig=True, savefig=False):
@@ -77,16 +73,11 @@ def sc_convergence(showfig=True, savefig=False):
             var_mISI = data[1]['V'].ravel()
             skew_mISI = data[2]['S'].ravel()
             kurt_mISI = data[3]['K'].ravel()
-            ax[0, cv_idx].plot(K_LIST, mean_mISI, figmarks[sc_idx],
-                markevery=3,
-                label=f'$\\langle mISI \\rangle$, ${sc}$ SC points')
+            ax[0, cv_idx].plot(K_LIST, mean_mISI, figmarks[sc_idx], markevery=3, label=f'$\\langle mISI \\rangle$, ${sc}$ SC points')
             ax[0, cv_idx].set_title(f'$\\alpha = {cv}$%')
-            ax[1, cv_idx].plot(K_LIST, var_mISI, figmarks[sc_idx],
-                markevery=3, label=f'$Var(mISI)$, ${sc}$ SC points')
-            ax[2, cv_idx].plot(K_LIST, skew_mISI, figmarks[sc_idx],
-                markevery=3, label=f'$Skew(mISI)$, ${sc}$ SC points')
-            ax[3, cv_idx].plot(K_LIST, kurt_mISI, figmarks[sc_idx],
-                markevery=3, label=f'$Kurt(mISI)$, ${sc}$ SC points')
+            ax[1, cv_idx].plot(K_LIST, var_mISI, figmarks[sc_idx], markevery=3, label=f'$Var(mISI)$, ${sc}$ SC points')
+            ax[2, cv_idx].plot(K_LIST, skew_mISI, figmarks[sc_idx], markevery=3, label=f'$Skew(mISI)$, ${sc}$ SC points')
+            ax[3, cv_idx].plot(K_LIST, kurt_mISI, figmarks[sc_idx], markevery=3, label=f'$Kurt(mISI)$, ${sc}$ SC points')
             ax[3, cv_idx].set_xlabel('$k$')
             if cv_idx == 0:
                 ax[0, cv_idx].set_ylabel('$mISI (k)$ [ms]')
@@ -101,27 +92,23 @@ def sc_convergence(showfig=True, savefig=False):
     if showfig:
         plt.show()
     if savefig:
-        fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_convergence.eps'),
-            format='eps', bbox_inches='tight')
+        fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_convergence.eps'), format='eps', bbox_inches='tight')
 
 
 def anova(showfig=True, savefig=False):
     from cycler import cycler
-    plt.rcParams.update({
-        'axes.prop_cycle': cycler('linestyle', ['-', ':', '--'])})
+    plt.rcParams.update({'axes.prop_cycle': cycler('linestyle', ['-', ':', '--'])})
     nfigs = len(CV_LIST)
     plotting_config(nfigs, 3)
     fig, ax = plt.subplots(3, nfigs, sharex=True, sharey=True, squeeze=True)
     sc = 5
     for cv_idx, cv in enumerate(CV_LIST):
-        data = loadmat(os.path.join(DATA_PATH, 
-            f'SA_ANOVA_indices_CI{cv}_SC_{sc}.mat'))['SS']
+        data = loadmat(os.path.join(DATA_PATH, f'SA_ANOVA_indices_CI{cv}_SC_{sc}.mat'))['SS']
         ax[0, cv_idx].plot(K_LIST, data[:, :3])
         ax[0, cv_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'])
         ax[0, cv_idx].set_title(f'${sc}$ SC points, $\\alpha = {cv}$%')
         ax[1, cv_idx].plot(K_LIST, data[:, 3:6])
-        ax[1, cv_idx].legend(['$g_{Na}$ $g_{K}$', '$g_{Na}$ $g_{L}$',
-            '$g_{K}$ $g_{L}$'])
+        ax[1, cv_idx].legend(['$g_{Na}$ $g_{K}$', '$g_{Na}$ $g_{L}$', '$g_{K}$ $g_{L}$'])
         ax[2, cv_idx].plot(K_LIST, data[:, 6:])
         ax[2, cv_idx].set_xlabel('$k$')
         ax[2, cv_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'], loc='lower right')
@@ -133,13 +120,12 @@ def anova(showfig=True, savefig=False):
     if showfig:
         plt.show()
     if savefig:
-        fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_anova.eps'),
-            format='eps', bbox_inches='tight')
+        fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_anova.eps'), format='eps', bbox_inches='tight')
 
 
 if __name__ == "__main__":
-    showfig = False
-    savefig = True
+    showfig = True
+    savefig = False
     multiview(showfig, savefig)
     sc_convergence(showfig, savefig)
     anova(showfig, savefig)
