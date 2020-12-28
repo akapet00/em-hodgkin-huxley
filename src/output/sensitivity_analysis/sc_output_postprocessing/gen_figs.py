ef65f32b8e557eb1a39a8493603b4fe104d2ba68
antelk <alojic00@fesb.hr>
2020-12-28 09:34:11 +0100

    update script and rerun experiments for alpha=[5, 10, 20]

--- src/output/sensitivity_analysis/sc_output_postprocessing/gen_figs.py
+++ src/output/sensitivity_analysis/sc_output_postprocessing/gen_figs.py
@@ -5,10 +5,10 @@ import numpy as np
 from scipy.io import loadmat
 
 
-DATA_PATH = 'mean_ISI_tsim-300_tIinj-0-300_A-5_noise-1_T-6.3_k-0-5'
-K_LIST = np.linspace(0, 5, 50)
+DATA_PATH = 'mean_ISI_tsim-300_tIinj-0-300_A-10_noise-0_T-15_k-0-2'
+K_LIST = np.linspace(0, 2, 50)
 SCP_LIST = [3, 5, 7, 9]
-CV_LIST = [5, 10, 20, 50]
+CV_LIST = [5, 10, 20]
 
 
 def plotting_config(nrows=1, ncols=1):
@@ -34,6 +34,28 @@ def load_data(ci, sc):
     return (mean_mat, var_mat, skew_mat, kurt_mat)
 
 
+def singleview(showfig=True, savefig=False):
+    plotting_config()
+    sc = 5
+    for cv in CV_LIST:
+        data = load_data(cv, sc)
+        mean_mISI = data[0]['M'].ravel()
+        var_mISI = data [1]['V'].ravel()
+        lb = mean_mISI - np.sqrt(var_mISI)
+        ub = mean_mISI + np.sqrt(var_mISI)
+        fig, ax = plt.subplots()
+        ax.plot(K_LIST, mean_mISI, color='black', label=f'$\\langle mISI \\rangle$, ${sc}$ SC points, $\\alpha = {cv}$%')
+        ax.fill_between(K_LIST, lb, ub, color='lightgray', label='95% CI')
+        ax.set_xlabel('$k$')
+        ax.set_ylabel('$mISI (k)$ [ms]')
+        ax.legend(loc='best')
+        plt.tight_layout()
+        if showfig:
+            plt.show()
+        if savefig:
+            fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_singleview_CI{cv}_SC_{sc}.eps'), format='eps', bbox_inches='tight')
+
+            
 def multiview(showfig=True, savefig=False):
     nrows = len(CV_LIST)
     ncols = len(SCP_LIST)
@@ -64,30 +86,30 @@ def multiview(showfig=True, savefig=False):
 def sc_convergence(showfig=True, savefig=False):
     figmarks = ['ko-', 'ks-', 'k^:', 'kv:']
     nfigs = len(CV_LIST)
-    plotting_config(nfigs, 4)
-    fig, ax = plt.subplots(4, nfigs, sharex=True, squeeze=True)
+    plotting_config(nfigs, 2)
+    fig, ax = plt.subplots(2, nfigs, sharex=True, squeeze=True)
     for cv_idx, cv in enumerate(CV_LIST):
         for sc_idx, sc in enumerate(SCP_LIST):
             data = load_data(cv, sc) 
             mean_mISI = data[0]['M'].ravel()
             var_mISI = data[1]['V'].ravel()
-            skew_mISI = data[2]['S'].ravel()
-            kurt_mISI = data[3]['K'].ravel()
-            ax[0, cv_idx].plot(K_LIST, mean_mISI, figmarks[sc_idx], markevery=3, label=f'$\\langle mISI \\rangle$, ${sc}$ SC points')
+            # skew_mISI = data[2]['S'].ravel()
+            # kurt_mISI = data[3]['K'].ravel()
+            ax[0, cv_idx].plot(K_LIST, mean_mISI, figmarks[sc_idx], markevery=2, label=f'$\\langle mISI \\rangle$, ${sc}$ SC points')
             ax[0, cv_idx].set_title(f'$\\alpha = {cv}$%')
-            ax[1, cv_idx].plot(K_LIST, var_mISI, figmarks[sc_idx], markevery=3, label=f'$Var(mISI)$, ${sc}$ SC points')
-            ax[2, cv_idx].plot(K_LIST, skew_mISI, figmarks[sc_idx], markevery=3, label=f'$Skew(mISI)$, ${sc}$ SC points')
-            ax[3, cv_idx].plot(K_LIST, kurt_mISI, figmarks[sc_idx], markevery=3, label=f'$Kurt(mISI)$, ${sc}$ SC points')
-            ax[3, cv_idx].set_xlabel('$k$')
+            ax[1, cv_idx].plot(K_LIST, var_mISI, figmarks[sc_idx], markevery=2, label=f'$Var(mISI)$, ${sc}$ SC points')
+            # ax[2, cv_idx].plot(K_LIST, skew_mISI, figmarks[sc_idx], markevery=2, label=f'$Skew(mISI)$, ${sc}$ SC points')
+            # ax[3, cv_idx].plot(K_LIST, kurt_mISI, figmarks[sc_idx], markevery=2, label=f'$Kurt(mISI)$, ${sc}$ SC points')
+            ax[1, cv_idx].set_xlabel('$k$')
             if cv_idx == 0:
                 ax[0, cv_idx].set_ylabel('$mISI (k)$ [ms]')
                 ax[1, cv_idx].set_ylabel('$Var(mISI)(k)$ [ms]')
-                ax[2, cv_idx].set_ylabel('$Skew(mISI)(k)$ [ms]')
-                ax[3, cv_idx].set_ylabel('$Kurt(mISI)(k)$ [ms]')
+                # ax[2, cv_idx].set_ylabel('$Skew(mISI)(k)$ [ms]')
+                # ax[3, cv_idx].set_ylabel('$Kurt(mISI)(k)$ [ms]')
             ax[0, cv_idx].legend(loc='best')
             ax[1, cv_idx].legend(loc='best')  
-            ax[2, cv_idx].legend(loc='best')
-            ax[3, cv_idx].legend(loc='best')           
+            # ax[2, cv_idx].legend(loc='best')
+            # ax[3, cv_idx].legend(loc='best')           
     plt.tight_layout()
     if showfig:
         plt.show()
@@ -99,33 +121,32 @@ def anova(showfig=True, savefig=False):
     from cycler import cycler
     plt.rcParams.update({'axes.prop_cycle': cycler('linestyle', ['-', ':', '--'])})
     nfigs = len(CV_LIST)
-    plotting_config(nfigs, 3)
-    fig, ax = plt.subplots(3, nfigs, sharex=True, sharey=True, squeeze=True)
+    plotting_config(nfigs, 2)
     sc = 5
+    fig, ax = plt.subplots(2, nfigs, sharex=True, sharey=True, squeeze=True)
     for cv_idx, cv in enumerate(CV_LIST):
         data = loadmat(os.path.join(DATA_PATH, f'SA_ANOVA_indices_CI{cv}_SC_{sc}.mat'))['SS']
-        ax[0, cv_idx].plot(K_LIST, data[:, :3])
-        ax[0, cv_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'])
+        ax[0, cv_idx].plot(K_LIST, data[:, :3], color='black', linewidth=2)
+        ax[0, cv_idx].plot(K_LIST, data[:, 3:6], color='gray', linewidth=1)
+        ax[0, cv_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$', '$g_{Na}$ $g_{K}$', '$g_{Na}$ $g_{L}$', '$g_{K}$ $g_{L}$'])
         ax[0, cv_idx].set_title(f'${sc}$ SC points, $\\alpha = {cv}$%')
-        ax[1, cv_idx].plot(K_LIST, data[:, 3:6])
-        ax[1, cv_idx].legend(['$g_{Na}$ $g_{K}$', '$g_{Na}$ $g_{L}$', '$g_{K}$ $g_{L}$'])
-        ax[2, cv_idx].plot(K_LIST, data[:, 6:])
-        ax[2, cv_idx].set_xlabel('$k$')
-        ax[2, cv_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'], loc='lower right')
+        ax[1, cv_idx].plot(K_LIST, data[:, 6:])
+        ax[1, cv_idx].set_xlabel('$k$')
+        ax[1, cv_idx].legend(['$g_{Na}$', '$g_{K}$', '$g_{L}$'], loc='lower right')
         if cv_idx == 0:
-            ax[0, cv_idx].set_ylabel('$1^{st}$ order sensitivity')
-            ax[1, cv_idx].set_ylabel('$2^{nd}$ order sensitivity')
-            ax[2, cv_idx].set_ylabel('total effect sensitivity')
+            ax[0, cv_idx].set_ylabel('$1^{st}$ and $2^{nd}$ order sensitivity')
+            ax[1, cv_idx].set_ylabel('total effect sensitivity')
     plt.tight_layout()
     if showfig:
         plt.show()
     if savefig:
-        fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_anova.eps'), format='eps', bbox_inches='tight')
+        fig.savefig(fname=os.path.join('figures', f'{DATA_PATH}_anova_SC_{sc}.eps'), format='eps', bbox_inches='tight')
 
 
 if __name__ == "__main__":
     showfig = False
     savefig = True
+    singleview(showfig, savefig)
     multiview(showfig, savefig)
     sc_convergence(showfig, savefig)
     anova(showfig, savefig)
