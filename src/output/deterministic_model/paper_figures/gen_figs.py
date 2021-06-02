a267eba758f210ffc27f463d34bae1c96abb1009
antelk <alojic00@fesb.hr>
2021-06-02 10:45:59 +0200

    add counts instead of PMF to histogram

--- src/output/deterministic_model/paper_figures/gen_figs.py
+++ src/output/deterministic_model/paper_figures/gen_figs.py
@@ -1,25 +1,26 @@
-import os
-
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 import numpy as np
 from scipy.io import loadmat
 
 
-MAT_PATH = 'entropy_tsim-1500_tIinj-0-1500_isPeriodic-0_T-6.3_k-0.1'
+MAT_PATH = 'entropy_tsim-1000_tIinj-0-1000_isPeriodic-0_T-6.3_k-0.1'
 
 
 def plotting_config(nrows=1, ncols=1):
-    plt.style.use('seaborn-paper')
     plt.rcParams.update({
-        'text.usetex': False,
+        'text.usetex': True,
         'font.family': 'serif',
-        'font.size': 10,
-        'axes.labelsize': 10,
-        'grid.linewidth': 0.7,
-        'legend.fontsize': 8,
-        'xtick.labelsize': 8,
-        'ytick.labelsize': 8,
-        'figure.figsize': [4.774*nrows, 2.950*ncols],
+        'font.size': 14,
+        'figure.figsize': (4.774 * ncols, 2.950 * nrows),
+        'lines.linewidth': 1,
+        'lines.dashed_pattern': (3, 5),
+        'axes.labelsize': 14,
+        'axes.titlesize': 14,
+        'grid.linewidth': 0.5,
+        'legend.fontsize': 14,
+        'xtick.labelsize': 14,
+        'ytick.labelsize': 14,
     })
 
 
@@ -33,17 +34,21 @@ def main(showfig, savefig):
     V = load_data(MAT_PATH, 'V')
     I = load_data(MAT_PATH, 'I')
     isi = load_data(MAT_PATH, 'isi')
-    iqr = np.percentile(isi, 75) - np.percentile(isi, 25)
-    n_bins = (isi.max() - isi.min()) / (2 * iqr * np.power(isi.size, -1/3))
-    hist, edges = np.histogram(isi, bins=int(n_bins))
-    bins = edges[:-1] + (edges[1] - edges[0]) / 2
-    p = hist / np.sum(hist)
-    entropy = - np.sum(p[np.where(p != 0)] * np.log(p[np.where(p != 0)]))
+    iqr = np.subtract(*np.percentile(isi, [75, 25]))
+    bin_width = (2 * iqr) / np.power(isi.size, 1/3)
+    isi_range = isi.max() - isi.min()
+    n_bins = int((isi_range / bin_width) + 1)
+    bin_step = isi_range / n_bins
+    bins = np.arange(isi.min(), isi.max() + bin_step, bin_step)
+    counts, _ = np.histogram(isi, bins=bins)
+    p = counts / np.sum(counts)
+    entropy = - np.sum(p[np.where(p > 0)] * np.log2(p[np.where(p > 0)]))
+    print(f'{entropy = :.4f}')
 
-    plotting_config(1.5, 1.5)
+    plotting_config(2, 2)
     fig = plt.figure()
     ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=2, rowspan=2)
-    ax1.plot(t, V , color='k')
+    ax1.plot(t, V, color='k')
     ax1.grid()
     ax1.set_ylabel('$V_m$ [mV]')
     ax2 = plt.subplot2grid((3, 3), (2, 0), colspan=2, sharex=ax1)
@@ -52,17 +57,20 @@ def main(showfig, savefig):
     ax2.set_xlabel('t [ms]')
     ax2.set_ylabel('$I_{ext}$ [$\\mu$A/cm$^2$]')
     ax3 = plt.subplot2grid((3, 3), (0, 2), rowspan=3)
-    ax3.bar(bins, p, 40.0/n_bins, color='black')
+    ax3.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    counts, edges, _ = ax3.hist(isi, 'sqrt', color='k', edgecolor='w')
+    ax3.set_xticks(edges[::3])
     ax3.set_xlabel('ISI [ms]')
-    ax3.set_ylabel('PMF')
-    ax3.set_title(f'$H = {round(entropy, 4)}$')
+    ax3.set_ylabel('count')
+    ax3.set_title(f'$H = {entropy:.4f}$')
     plt.tight_layout()
     if showfig:
         plt.show()
     if savefig:
         fig.savefig(fname=f'{MAT_PATH}.eps', format='eps', bbox_inches='tight')
 
+
 if __name__ == "__main__":
     showfig = False
-    savefig = True
+    savefig = False
     main(showfig, savefig)
